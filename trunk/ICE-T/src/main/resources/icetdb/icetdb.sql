35fb44866c3af1d619cae3ef774326bc5030decb
timothee.aufort@gmail.com <timothee.aufort@gmail.com@e649e4b3-26e7-d348-6c4a-2c898d3249e1>
2013-01-25 07:27:58 +0000

    SQL script corrections

--- trunk/ICE-T/src/main/resources/icetdb/icetdb.sql
+++ trunk/ICE-T/src/main/resources/icetdb/icetdb.sql
@@ -13,8 +13,8 @@ CREATE TABLE EntityM (
 	name VARCHAR(50) NOT NULL,
 	CombatEncounter_id INTEGER,
 	CEname VARCHAR(50),
-	CONSTRAINT PK_EntityM PRIMARY KEY (EntityM_id, name)
-	CONSTRAINT FK_CombatEncounter FOREIGN KEY (CombatEncounter_id, CEname) REFERENCES CombatEncounter (CombatEncounter_id, CEname)
+	CONSTRAINT PK_EntityM PRIMARY KEY (EntityM_id, name),
+	CONSTRAINT FK_EntityM_CombatEncounter FOREIGN KEY (CombatEncounter_id, CEname) REFERENCES CombatEncounter (CombatEncounter_id, CEname)
 )
 
 INSERT INTO EntityM VALUES (1, 'Effect1', 1, 'CE1'); 
@@ -72,35 +72,18 @@ CREATE TABLE CharacterSheet (
 	misc VARCHAR(1000),
 	keywords VARCHAR(1000),
 	powerSource VARCHAR(1000),
-	CONSTRAINT FK_EntityM FOREIGN KEY (EntityM_id, name) REFERENCES EntityM (EntityM_id, name)
+	CONSTRAINT FK_CharacterSheet_EntityM FOREIGN KEY (EntityM_id, name) REFERENCES EntityM (EntityM_id, name)
 )
 
 INSERT INTO CharacterSheet VALUES (1, 7, 'CharacterSheet1', 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 'big', 1, 2, 3, 'french, english', 'misc', 'hi, hello', 'earth'); 
 INSERT INTO CharacterSheet VALUES (2, 8, 'CharacterSheet2', 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 'spiky', 1, 2, 3, 'japonese, chinese', 'misc', 'hey', 'fire'); 
 INSERT INTO CharacterSheet VALUES (3, 9, 'CharacterSheet3', 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 'small', 1, 2, 3, 'no language', 'misc', 'lol', 'wind'); 
 
-CREATE TABLE Effect (
-	Effect_id INTEGER NOT NULL PRIMARY KEY,
-	EntityM_id INTEGER,
-	name VARCHAR(50),
-	Creature_id INTEGER,
-	player_name VARCHAR(50),
-	changes VARCHAR(500),
-	damage INTEGER,
-	metrics VARCHAR(500),
-	CONSTRAINT FK_EntityM FOREIGN KEY (EntityM_id, name) REFERENCES EntityM (EntityM_id, name),
-	CONSTRAINT FK_Creature FOREIGN KEY (Creature_id, player_name) REFERENCES Creature (Creature_id, player_name)
-)
-
-INSERT INTO Effect VALUES (1, 1, 'Effect1', 1, 'Tim', 'Damage value rises for every player by 1', 10, 'cool'); 
-INSERT INTO Effect VALUES (2, 2, 'Effect2', 3, 'Bill', 'The effect lasts 10 turns', 15, 'not so long');
-INSERT INTO Effect VALUES (3, 2, 'Effect2', 2, 'James', 'The effect lasts 10 turns', 15, 'not so long');
-
 CREATE TABLE Team (
 	Team_id INTEGER NOT NULL PRIMARY KEY,
 	EntityM_id INTEGER UNIQUE,
 	name VARCHAR(50) UNIQUE,
-	CONSTRAINT FK_EntityM FOREIGN KEY (EntityM_id, name) REFERENCES EntityM (EntityM_id, name)
+	CONSTRAINT FK_Team_EntityM FOREIGN KEY (EntityM_id, name) REFERENCES EntityM (EntityM_id, name)
 )
 
 INSERT INTO Team VALUES (1, 3, 'Team1'); 
@@ -118,10 +101,10 @@ CREATE TABLE TrapHazard (
 	xp INTEGER,
 	difficultyLevel INTEGER,
 	counterMeasureDescription VARCHAR(500),
-	CONSTRAINT FK_EntityM FOREIGN KEY (EntityM_id, name) REFERENCES EntityM (EntityM_id, name)
+	CONSTRAINT FK_TrapHazard_EntityM FOREIGN KEY (EntityM_id, name) REFERENCES EntityM (EntityM_id, name)
 )
 
-INSERT INTO TrapHazard VALUES (1, 5, 'TrapHazard1', 12, 20 'Damage value rises by 1 every turn', 'player', 12, 36, 11, 'no counter measure'); 
+INSERT INTO TrapHazard VALUES (1, 5, 'TrapHazard1', 12, 20, 'Damage value rises by 1 every turn', 'player', 12, 36, 11, 'no counter measure'); 
 INSERT INTO TrapHazard VALUES (2, 6, 'TrapHazard2', 5, 30, 'The trap only lasts 2 hours', 'player', 5, 89, 50, 'no counter measure');
 
 CREATE TABLE Creature (
@@ -142,10 +125,27 @@ INSERT INTO Creature VALUES (2, 'James', 2, 1, 10, 8, 2, FALSE, 18);
 INSERT INTO Creature VALUES (3, 'Bill', 3, 2, 12, 10, 1, TRUE, 29);  
 INSERT INTO Creature VALUES (4, 'Tim', 1, 2, 30, 5, 5, TRUE, 10);
 
+CREATE TABLE Effect (
+	Effect_id INTEGER NOT NULL PRIMARY KEY,
+	EntityM_id INTEGER,
+	name VARCHAR(50),
+	Creature_id INTEGER,
+	player_name VARCHAR(50),
+	changes VARCHAR(500),
+	damage INTEGER,
+	metrics VARCHAR(500),
+	CONSTRAINT FK_Effect_EntityM FOREIGN KEY (EntityM_id, name) REFERENCES EntityM (EntityM_id, name),
+	CONSTRAINT FK_Effect_Creature FOREIGN KEY (Creature_id, player_name) REFERENCES Creature (Creature_id, player_name)
+)
+
+INSERT INTO Effect VALUES (1, 1, 'Effect1', 1, 'Tim', 'Damage value rises for every player by 1', 10, 'cool'); 
+INSERT INTO Effect VALUES (2, 2, 'Effect2', 3, 'Bill', 'The effect lasts 10 turns', 15, 'not so long');
+INSERT INTO Effect VALUES (3, 2, 'Effect2', 2, 'James', 'The effect lasts 10 turns', 15, 'not so long');
+
 CREATE TABLE Attack (
 	Attack_id INTEGER NOT NULL PRIMARY KEY,
 	Creature_id INTEGER,
-	player_name VARCHAR(50)
+	player_name VARCHAR(50),
 	primaryTarget VARCHAR(50),
 	secondaryTarget VARCHAR(50),
 	accessories VARCHAR(1000),
@@ -154,8 +154,8 @@ CREATE TABLE Attack (
 	hit VARCHAR(50),
 	miss VARCHAR(50),
 	basic BOOLEAN DEFAULT TRUE,
-	triggers VARCHAR(50),
-	CONSTRAINT FK_Creature FOREIGN KEY (Creature_id, player_name) REFERENCES Creature (Creature_id, player_name)
+	triggers VARCHAR(1000),
+	CONSTRAINT FK_Attack_Creature FOREIGN KEY (Creature_id, player_name) REFERENCES Creature (Creature_id, player_name)
 )
 
 INSERT INTO Attack VALUES (1, 1, 'Tim', 'James', 'Tim', 'knife & gun', 'arcane', 2, '2D16', '1D32', TRUE, 'the player cannot use that attack on his/her first turn');
@@ -167,7 +167,6 @@ INSERT INTO Attack VALUES (6, 3, 'Bill', 'Tim', 'James', 'MachineGun', 'martial'
 INSERT INTO Attack VALUES (7, 1, 'Tim', 'James', 'Tim', 'knife & gun', 'arcane', 2, '2D16', '1D32', TRUE, 'the player cannot use that attack on his/her first turn');
 INSERT INTO Attack VALUES (8, 2, 'James', 'Bill', 'Tim', 'knife & gun', 'arcane', 2, '2D16', '1D32', TRUE, 'the player cannot use that attack on his/her first turn');
 INSERT INTO Attack VALUES (9, 3, 'Bill', 'James', 'Tim', 'knife & gun', 'arcane', 2, '2D16', '1D32', TRUE, 'the player cannot use that attack on his/her first turn');
-
 
 CREATE TABLE Attack_Type (
 	Attack_Type_id INTEGER NOT NULL PRIMARY KEY,
@@ -231,11 +230,11 @@ CREATE TABLE Rewards (
 	CEname VARCHAR(50),
 	XP INTEGER,
 	treasure VARCHAR(200),
-	CONSTRAINT FK_CombatEncounter FOREIGN KEY (CombatEncounter_id, CEname) REFERENCES CombatEncounter (CombatEncounter_id, CEname) 
+	CONSTRAINT FK_Rewards_CombatEncounter FOREIGN KEY (CombatEncounter_id, CEname) REFERENCES CombatEncounter (CombatEncounter_id, CEname) 
 )
 
-INSERT INTO Rewards VALUES (1, 2, 'CE1', 500, 'Gold'); 
-INSERT INTO Rewards VALUES (2, 1, 'CE2', 1, 'Pen'); 
+INSERT INTO Rewards VALUES (1, 2, 'CE2', 500, 'Gold'); 
+INSERT INTO Rewards VALUES (2, 1, 'CE1', 1, 'Pen'); 
 
 CREATE TABLE Tally (
 	Tally_id INTEGER NOT NULL,
@@ -243,7 +242,7 @@ CREATE TABLE Tally (
 	CombatEncounter_id INTEGER UNIQUE,
 	CEname VARCHAR(50) UNIQUE,
 	CONSTRAINT PK_Tally PRIMARY KEY (Tally_id, name),
-	CONSTRAINT FK_CombatEncounter FOREIGN KEY (CombatEncounter_id, CEname) REFERENCES CombatEncounter (CombatEncounter_id, CEname) 
+	CONSTRAINT FK_Tally_CombatEncounter FOREIGN KEY (CombatEncounter_id, CEname) REFERENCES CombatEncounter (CombatEncounter_id, CEname) 
 )
 
 INSERT INTO Tally VALUES (1,'Tally1', 1, 'CE1'); 
@@ -257,7 +256,7 @@ CREATE TABLE Tuple (
 	value1 INTEGER,
 	value2 INTEGER,
 	CONSTRAINT PK_Tuple PRIMARY KEY (Tuple_id, Tuple_name),
-	CONSTRAINT FK_Tally FOREIGN KEY (Tally_id, name) REFERENCES Tally (Tally_id, name)
+	CONSTRAINT FK_Tuple_Tally FOREIGN KEY (Tally_id, name) REFERENCES Tally (Tally_id, name)
 )
 
 INSERT INTO Tuple VALUES (1,'Tuple1', 1, 'Tally1', 5, 8); 
@@ -276,8 +275,8 @@ CREATE TABLE Stats (
 	hits INTEGER,
 	misses INTEGER,
 	assists INTEGER,
-	CONSTRAINT FK_CombatEncounter FOREIGN KEY (CombatEncounter_id, CEname) REFERENCES CombatEncounter (CombatEncounter_id, CEname),
-	CONSTRAINT FK_Creature FOREIGN KEY (Creature_id, player_name) REFERENCES Creature (Creature_id, player_name)
+	CONSTRAINT FK_Stats_CombatEncounter FOREIGN KEY (CombatEncounter_id, CEname) REFERENCES CombatEncounter (CombatEncounter_id, CEname),
+	CONSTRAINT FK_Stats_Creature FOREIGN KEY (Creature_id, player_name) REFERENCES Creature (Creature_id, player_name)
 )
 
 INSERT INTO Stats VALUES (1, 1, 'CE1', 1, 'Tim', 5, 8, 10, 12, 19); 
